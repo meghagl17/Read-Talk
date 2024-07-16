556ce4d26184170fc4a924d0df7cc2447e1b0064
meghagl17 <megha.agl17@gmail.com>
2024-07-16 00:40:26 -0700

    fixed a typo

--- src/app/api/webhooks/clerk/route.js
+++ src/app/api/webhooks/clerk/route.js
@@ -62,7 +62,7 @@ export async function POST(req) {
 
   // CREATE user in mongoDB
   if (eventType === "user.created") {
-    const { id, email_address, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
         clerkId: id,
