5c6203d1bc46ac36fb15813f475268ad02b301d6
meghagl17 <megha.agl17@gmail.com>
2024-07-15 22:28:04 -0700

    started working on mondodb and authentication

---
+++ src/lib/db.js
@@ -0,0 +1,26 @@
+import mongoose, { Moongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL
+
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = {
+        conn: null, 
+        promise: null,
+    };
+}
+
+export const connent = async () => {
+    if (cached.conn) {
+        return cached.conn;
+    }
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+        dbName: 'bookTalk-db',
+        bufferCommands: false, 
+        connectTimeoutMS: 30000
+    })
+
+    cached.conn = await chached.promise;
+    return cached.conn;
+}
