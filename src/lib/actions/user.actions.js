9615e3556bd95fdbad0be126387babe35a5dd874
meghagl17 <megha.agl17@gmail.com>
2024-07-17 11:38:58 -0700

    made changes to some files

--- src/lib/actions/user.actions.js
+++ src/lib/actions/user.actions.js
@@ -1,16 +1,14 @@
-"use server"
+"use server";
 
-import User from "../modals/user.modal"
-import { connect } from "../db"
+import User from "@/lib/modals/user.modal";
+import { connect } from "@/lib/db";
 
-export async function createUser(user){
-    try{
-        await connect();
-        const newUser = await User.create(user);
-        console.log("in user.actions.js", JSON.stringify(newUser));
-        return JSON.parse(JSON.stringify(newUser));
-    } catch (error){
-        console.log("error in .actions file");
-        console.log(error);
-    }
+export async function createUser(user) {
+  try {
+    await connect();
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,31 +1,33 @@
-import mongoose, { Moongoose } from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL!;
 
-if (!MONGODB_URL) {
-  throw new Error('Please define the MONGODB_URL environment variable inside .env.local');
+interface MongooseConn {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached = global.mongoose;
+let cached: MongooseConn = (global as any).mongoose;
 
-if(!cached){
-    cached = global.mongoose = {
-        conn: null, 
-        promise: null,
-    };
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connect = async () => {
-    if (cached.conn) {
-        return cached.conn;
-    }
-    
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-        dbName: 'BookTalk',
-        bufferCommands: false, 
-        connectTimeoutMS: 30000
-    })
+  if (cached.conn) return cached.conn;
 
-    cached.conn = await cached.promise;
-    return cached.conn;
-}
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "bookTalk",
+      bufferCommands: false,
+      connectTimeoutMS: 30000,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
